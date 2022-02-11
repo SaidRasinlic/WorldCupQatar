1de908cdf482c6b9aeb2d66f52cd8143f836fffb
SaidRasinlic <saidrasinlic.jr@gmail.com>
2022-02-11 16:28:32 +0100

    Update

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+$(document).click(function(event) {
+
+ let clickover = $(event.target);
+ let navbar = $('.navbar-collapse');
+ let opened = navbar.hasClass("show");
+ if (opened === true && !clickover.hasClass('navbar-toggler')) {
+  navbar.collapse('hide');
+ }
+});
+
+$('.navbar').click(function(e) {
+ e.stopPropagation();
+})
